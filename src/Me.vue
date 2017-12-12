0c9ebcc3ba21b91705aec535d9feb04d30a7ce43
Punk1ee <473320837@qq.com>
2017-12-12 18:52:21 +0800

    更新了个人中心页面，还剩下表单验证和本地存取账号未做

--- src/Me.vue
+++ src/Me.vue
@@ -1,22 +1,68 @@
 <template>
     <div class="me">
-        <!-- loaged -->
-        <me-loged
-            :loged="loged"
-            :nickname="nickname"
-            :mainList="mainList"
-            checkoutLog="退出登录"
-            register="注册"
-            toLog="登录"
-        />
-        
+        <div v-if="loged" class="loged-box">
+            <div class="user-box me-header loged">
+                <div class="pic-box"></div>
+                <p class="nickname">{{nickname}}</p>  
+            </div>
+            <div class="me-main">
+                <ul>
+                    <li v-for="item in mainList" :key="item.id">
+                        <span>{{item}}</span>
+                        <i></i>
+                    </li>
+                </ul>  
+                <form>
+                    <button type="button" class="btn">退出登录</button>    
+                </form>  
+            </div> 
+        </div>
+        <div v-else class="not-loged-box">
+            <div class="user-box me-header notLoged">
+                <div class="pic-box"></div>
+                <span class="register" :class="{pageActive: showReg}" @click="showRegOrLog">注册</span>&nbsp;&nbsp;
+                <span class="to-log" :class="{pageActive: showLog}" @click="showRegOrLog">登录</span>
+            </div>
+            <form v-if="showReg" class="reg-box">
+                <div class="form-group">
+                    <label for="">账号：</label>
+                    <input type="text" placeholder="请输入账号" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">密码：</label>
+                    <input type="password" placeholder="请输入密码" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">确认密码：</label>
+                    <input type="password" placeholder="请确认密码" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button type="button" class="btn">注册</button>
+                </div>
+            </form>
+            <form v-else class="log-box">
+                <div class="form-group">
+                    <label for="">账号：</label>
+                    <input type="text" placeholder="请输入账号" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">密码：</label>
+                    <input type="password" placeholder="请输入密码" class="form-control">
+                </div>
+                <div class="form-group">
+                    <button type="button" class="btn">登录</button>
+                </div>
+            </form>
+            
+        </div>
     </div>
-
 </template>
 
 <script src="./scripts/me.js"></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-    
+    @import "./styles/me.less";
+
 </style>
+
--- src/scripts/me.js
+++ src/scripts/me.js
@@ -1,22 +1,29 @@
 // 导入子组件
-import MeLoged from '../components/MeLoged.vue'
 
 
 
 export default{
-    name: "Home",
+    name: "Me",
     components: {
-        MeLoged
+        
     },
     data() {
         return{
             loged: false,
             nickname: "punk1ee",
-            mainList: ["设置个人资料","收货地址管理","我的订单","我的评价","商品收藏","店铺收藏","浏览记录"]
+            mainList: ["设置个人资料","收货地址管理","我的订单","我的评价","商品收藏","店铺收藏","浏览记录"],
+            showReg: true,
+            showLog: false
         }
     },
     computed: {
 
+    },
+    methods: {
+        showRegOrLog() {
+            this.showReg = !this.showReg;   
+            this.showLog = !this.showLog;   
+        }
     }
 }
 
