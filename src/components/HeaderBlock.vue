d1210e4ee5f1dc200d2c611ffbec136a402def94
Punk1ee <473320837@qq.com>
2017-12-15 16:53:45 +0800

    完善了个人中心和购物车页面by punk1ee

---
+++ src/components/HeaderBlock.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="header-block">
+        <h4>{{headerText}}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderBlock",
+        props: ['headerText']
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .header-block{
+        height: 50px;
+        text-align: center;
+        background-color: #1a780d;
+        color: #fff;
+    }
+    .header-block > h4{
+        font-weight: normal;
+        line-height: 50px;
+    }
+</style>
