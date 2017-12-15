7373964bfa45f1c4eba31ba820e9637c7c2c2eb9
jackLiL <1123738375@qq.com>
2017-12-15 15:44:16 +0800

    -m
    修改了全局注册文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootCss from 'bootcss'
+import Bootstrap from 'bootstrap'
+
 
 
 Vue.config.productionTip = false
