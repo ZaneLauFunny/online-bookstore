8291ef09f506162ce5572f0aab41515fae6fbb6e
jackLiL <1123738375@qq.com>
2017-12-15 15:39:22 +0800

    修改了购物车页面

--- src/Cart.vue
+++ src/Cart.vue
@@ -1,7 +1,28 @@
 <template>
     <div class="cart">
-        
-    
+        <div class="header">
+            <h2>购物车</h2>
+        </div>
+        <div class="showNone" v-if="book">
+            <p>购物车内完全没有东西</p>
+            <router-link :to="{name: 'home'}" tag="a">去逛逛?</router-link>
+        </div>
+        <ul class="showBlock" v-else>
+            <li>
+                <div>
+                    <img src="../src/assets/logo.png">
+                </div>
+                <div>
+                    <label>11111111111111</label>
+                </div>
+                <div>
+                    <label>111111111</label>
+                </div>
+                <div>
+                    <label>$&</label>
+                </div>
+            </li>         
+        </ul>   
     </div>
 </template>
 
@@ -10,13 +31,19 @@
         name: "Cart",
         data() {
             return {
-                
+                newList:[],
+                book:false
             };
+        },
+        methods:{
+            addList:function(){
+
+            }
         }
     };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    
+<style lang="less" scoped>
+    @import "./styles/cart.less";
 </style>
